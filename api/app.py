f1db7860f628f7a90fad7ee6b9cfc684005fa21f
Pranshav Patel <96743783+pranshavpatel@users.noreply.github.com>
2025-09-13 20:18:28 -0400

    added a few details for live demo purpose

--- api/app.py
+++ api/app.py
@@ -10,9 +10,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="PolicyBot API")
 
+origins = [
+    "http://localhost:5173",
+    "https://pranshavpatel.vercel.app" 
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
